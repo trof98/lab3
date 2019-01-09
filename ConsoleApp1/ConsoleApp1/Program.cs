5fa9712320e8a1679e746360f7dccbe1f4576cd6
trof98 <trofi.98@ya.ru>
2019-01-10 01:22:57 +0700

    Подготовка к разделению функционала

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -15,44 +15,11 @@ namespace ConsoleApp1
             Console.WriteLine("Введите число B");
             int B = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Выберете пункт меню:");
-            Console.WriteLine("1 - умножение");
-            Console.WriteLine("2 - сложение");
-            Console.WriteLine("3 - деление");
-            Console.WriteLine("4 - вычитаение");
+            
             int num = Convert.ToInt32(Console.ReadLine());
             switch (num)
             {
-                case 1:
-                    {
-                        int C = A * B;
-                        Console.WriteLine("Ответ: {0}", C);
-                       
-                        break;
-                    }
-
-                case 2:
-                    {
-                        int C = A + B;
-                        Console.WriteLine("Ответ: {0}", C);
-
-                        break;
-                    }
-
-                case 3:
-                    {
-                        int C = A / B;
-                        Console.WriteLine("Ответ: {0}", C);
-
-                        break;
-                    }
-
-                case 4:
-                    {
-                        int C = A - B;
-                        Console.WriteLine("Ответ: {0}", C);
-
-                        break;
-                    }
+              
 
             }
             Console.ReadKey();
