a1ccdede3f943567d80f4f631fdbce003e7d8f9b
trof98 <trofi.98@ya.ru>
2019-01-10 01:54:52 +0700

    fix

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -15,7 +15,7 @@ namespace ConsoleApp1
             Console.WriteLine("Введите число B");
             int B = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Выберете пункт меню:");
-            Console.WriteLine("1 - умножение");
+            //Console.WriteLine("1 - умножение");
             //Console.WriteLine("2 - сложение");
             Console.WriteLine("3 - деление");
             //Console.WriteLine("4 - вычитаение");
@@ -23,13 +23,13 @@ namespace ConsoleApp1
             int num = Convert.ToInt32(Console.ReadLine());
             switch (num)
             {
-                case 1:
-                    {
-                        int C = A * B;
-                        Console.WriteLine("Ответ: {0}", C);
+                //case 1:
+                //    {
+                //        int C = A * B;
+                //        Console.WriteLine("Ответ: {0}", C);
 
-                        break;
-                    }
+                //        break;
+                //    }
 
                 //case 2:
                 //    {
