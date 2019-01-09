a053b1c7c20ff582e9c7aa7be3a76a0cd0d8b4fe
trof98 <trofi.98@ya.ru>
2019-01-10 01:55:14 +0700

    fix

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -15,45 +15,45 @@ namespace ConsoleApp1
             Console.WriteLine("Введите число B");
             int B = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Выберете пункт меню:");
-            Console.WriteLine("1 - умножение");
-            //Console.WriteLine("2 - сложение");
+            //Console.WriteLine("1 - умножение");
+            Console.WriteLine("2 - сложение");
             //Console.WriteLine("3 - деление");
             //Console.WriteLine("4 - вычитаение");
 
             int num = Convert.ToInt32(Console.ReadLine());
             switch (num)
             {
-                case 1:
+                //case 1:
+                //    {
+                //        int C = A * B;
+                //        Console.WriteLine("Ответ: {0}", C);
+
+                //        break;
+                //    }
+
+                case 2:
                     {
-                        int C = A * B;
+                        int C = A + B;
                         Console.WriteLine("Ответ: {0}", C);
 
                         break;
                     }
 
-                //case 2:
-                //    {
-                //        int C = A + B;
-                //        Console.WriteLine("Ответ: {0}", C);
+                    //case 3:
+                    //    {
+                    //        int C = A / B;
+                    //        Console.WriteLine("Ответ: {0}", C);
 
-                //        break;
-                //    }
+                    //        break;
+                    //    }
 
-                //case 3:
-                //    {
-                //        int C = A / B;
-                //        Console.WriteLine("Ответ: {0}", C);
+                    //case 4:
+                    //    {
+                    //        int C = A - B;
+                    //        Console.WriteLine("Ответ: {0}", C);
 
-                //        break;
-                //    }
-
-                //case 4:
-                //    {
-                //        int C = A - B;
-                //        Console.WriteLine("Ответ: {0}", C);
-
-                //        break;
-                //    }
+                    //        break;
+                    //    }
 
             }
             Console.ReadKey();
